ef88ae966f5bd832fc0620fe02a328fab1c0087f
MrFatra <ramadhanfatra2004@gmail.com>
2024-12-12 10:26:31 +0700

    init

---
+++ MODUL 4 & 5/js_M4_3.js
@@ -0,0 +1,12 @@
+function pesan {
+    alert("Ini Latihan Pertama Saya!");
+}
+
+function pesanConsole() {
+    console.log("Its Work");
+}
+
+function pesanDok() {
+    document.write("Succes<br>");
+    document.write("<a href=M4_2.html>Kembali</a>");
+}
